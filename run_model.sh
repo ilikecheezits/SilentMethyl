3f1e4ea6f194cd152384516d09e28fc1718c47cc
Samuel Zhang <szhang37@v004.ib.bridges2.psc.edu>
2026-05-16 06:53:39 -0400

    temp

--- run_model.sh
+++ run_model.sh
@@ -1,4 +1,4 @@
-c#!/bin/bash
+#!/bin/bash
 #SBATCH --job-name=SilentMethyl
 #SBATCH --partition=GPU-shared
 #SBATCH --nodes=1
@@ -12,4 +12,4 @@ module load anaconda3
 module load cuda/12.4.0
 conda activate silentmethyl
 
-python scripts/02_train_model.py --matrix_path data/processed/cleaned_training_data.csv --dict_path data/raw/DNA_Sequence_Dictionary.csv --region_vocab_path data/processed/SilentMethyl_RegionVocab.pkl --island_vocab_path data/processed/SilentMethyl_IslandVocab.pkl --save_dir ./checkpoints --batch_size 16 --steps_per_epoch 5000 --epochs 10 --lr 1e-4 --num_workers 4
+python scripts/02_train_model.py --matrix_path data/processed/cleaned_training_data.csv --dict_path data/raw/DNA_Sequence_Dictionary.csv --region_vocab_path data/processed/SilentMethyl_RegionVocab.pkl --island_vocab_path data/processed/SilentMethyl_IslandVocab.pkl --save_dir ./checkpoints --batch_size 16 --steps_per_epoch 2000 --epochs 10 --lr 1e-4 --num_workers 4
